c6f6ef676f1e0de7a1ffab3752a4b207b5465d7d
francomercado93 <mercadofranco.a@gmail.com>
2020-06-19 20:20:28 -0300

    Fix de usuario

--- src/app/domain/usuario.ts
+++ src/app/domain/usuario.ts
@@ -1,5 +1,5 @@
 export class Usuario {
     constructor(public usuario?: string, public alias?: string, public nombre?: string,
-        public apellido?: string, public password?: string, public esVidente?: boolean, public tipoUsuario?: number) {
+        public apellido?: string, public password?: string, public esVidente?: boolean, public tipoUsuario?: string, public provincia?: string, public ciudad?: string) {
     }
 }
