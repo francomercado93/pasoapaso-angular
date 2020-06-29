542229cc611282bed888432ad431f480e833a3ef
francomercado93 <mercadofranco.a@gmail.com>
2020-06-29 15:06:35 -0300

    Configuracion de baseUrl

--- src/app/configuration/baseUrl.ts
+++ src/app/configuration/baseUrl.ts
@@ -1,1 +1,8 @@
+// =======================Configuracion actual============================
 export const baseUrl = 'http://localhost:3000'
+
+// =======================Configuracion local============================
+// export const baseUrl = 'http://localhost:3000'
+
+// =======================Configuracion ngrok server(cambiar url que tira ngrok)============================
+// export const baseUrl = 'http://c9c9bdaece4a.ngrok.io'
